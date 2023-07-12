48df1cae8ece783269fdea252ca80540f542c0bf
unknown <vasanthgamer2000@gmail.com>
2023-07-12 15:16:16 +0530

    Added string to Integer, Date Time, Time Span using Parse, Try Parse, Convert

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_P2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //String to Integer using TryParse
+            string strNumber = "123";
+            int number;
+
+            try
+            {
+
+                Int32.TryParse(strNumber, out number);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("The string '" + strNumber + "' cannot be converted to an integer");
+
+            }
+
+            //String to DateTime Object
+            string strDateTime = "2023-07-12T12:34:56";
+            DateTime dateTime;
+
+            try
+            {
+                
+                dateTime = Convert.ToDateTime(strDateTime);
+            }
+            catch (Exception e)
+            {
+                
+                Console.WriteLine("The string '" + strDateTime + "' cannot be converted to a DateTime");
+            }
+
+            //String to Time Span Using Parse
+
+            string strTimeSpan = "01:02:03";
+            TimeSpan timeSpan;
+
+            try
+            {
+                
+                timeSpan = TimeSpan.Parse(strTimeSpan);
+            }
+            catch (Exception e)
+            {
+                
+                Console.WriteLine("The string '" + strTimeSpan + "' cannot be converted to a TimeSpan");
+            }
+
+
+        }
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Assignment3-P2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Assignment3-P2")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aa1708e6-6386-4359-a998-7ee6425a4956")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
